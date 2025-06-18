5fc28e942f27a83ff71df8293b249a6891042f06
Ashish Sahu <ashishsahu291@gmail.com>
2025-06-18 19:55:44 +0530

    sonar issue fixed for cognitive complexity

--- src/App.jsx
+++ src/App.jsx
@@ -7,47 +7,11 @@ import { ACTIONS } from "./utility/constants";
 function reducer(state, { type, payload }) {
   switch (type) {
     case ACTIONS.ADD_DIGIT:
-      if (state.overwrite) {
-        return {
-          ...state,
-          currentOperand: payload.digit,
-          overwrite: false,
-        };
-      }
-      if (payload.digit === "0" && state.currentOperand === "0") return state;
-      if (payload.digit === "." && state.currentOperand.includes(".")) {
-        return state;
-      }
-      return {
-        ...state,
-        currentOperand: `${state.currentOperand || ""}${payload.digit}`,
-      };
+      addDigit(state, payload);
+      break;
     case ACTIONS.CHOOSE_OPERATION:
-      if (state.currentOperand == null && state.previousOperand == null)
-        return state;
-
-      if (state.currentOperand == null) {
-        return {
-          ...state,
-          operation: payload.operation,
-        };
-      }
-
-      if (state.previousOperand == null) {
-        return {
-          ...state,
-          operation: payload.operation,
-          previousOperand: state.currentOperand,
-          currentOperand: null,
-        };
-      }
-
-      return {
-        ...state,
-        previousOperand: evaluate(state),
-        operation: payload.operation,
-        currentOperand: null,
-      };
+      chooseOperation(state, payload);
+      break;
     case ACTIONS.CLEAR:
       return {};
     case ACTIONS.EVALUATE:
@@ -67,21 +31,72 @@ function reducer(state, { type, payload }) {
         currentOperand: evaluate(state),
       };
     case ACTIONS.DELETE_DIGIT:
-      if (state.overwrite) {
-        return {
-          ...state,
-          overwrite: false,
-          currentOperand: null,
-        };
-      }
-      if (state.currentOperand == null) return state;
-      if (state.currentOperand.length === 1)
-        return { ...state, currentOperand: null };
-      return {
-        ...state,
-        currentOperand: state.currentOperand.slice(0, -1),
-      };
+      deleteDigit(state);
+      break;
   }
+}
+
+function addDigit(state, payload) {
+  if (state.overwrite) {
+    return {
+      ...state,
+      currentOperand: payload.digit,
+      overwrite: false,
+    };
+  }
+  if (payload.digit === "0" && state.currentOperand === "0") return state;
+  if (payload.digit === "." && state.currentOperand.includes(".")) {
+    return state;
+  }
+  return {
+    ...state,
+    currentOperand: `${state.currentOperand || ""}${payload.digit}`,
+  };
+}
+
+function chooseOperation(state, payload) {
+  if (state.currentOperand == null && state.previousOperand == null)
+    return state;
+
+  if (state.currentOperand == null) {
+    return {
+      ...state,
+      operation: payload.operation,
+    };
+  }
+
+  if (state.previousOperand == null) {
+    return {
+      ...state,
+      operation: payload.operation,
+      previousOperand: state.currentOperand,
+      currentOperand: null,
+    };
+  }
+
+  return {
+    ...state,
+    previousOperand: evaluate(state),
+    operation: payload.operation,
+    currentOperand: null,
+  };
+}
+
+function deleteDigit(state) {
+  if (state.overwrite) {
+    return {
+      ...state,
+      overwrite: false,
+      currentOperand: null,
+    };
+  }
+  if (state.currentOperand == null) return state;
+  if (state.currentOperand.length === 1)
+    return { ...state, currentOperand: null };
+  return {
+    ...state,
+    currentOperand: state.currentOperand.slice(0, -1),
+  };
 }
 
 function evaluate({ currentOperand, previousOperand, operation }) {
